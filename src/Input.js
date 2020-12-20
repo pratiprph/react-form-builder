43e72d1aed9be581e5907c448114f838fa91a425
pratiprph <pratipsinha@gmail.com>
2020-12-20 14:55:47 +0530

    first commit

---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Input() {
+    return (
+        <div>
+            <input type="email" placeholder="Enter Email"/>
+        </div>
+    )
+}
+
+export default Input;
---
+++ src/Password.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Password() {
+    return (
+        <div>
+            <input type="password" placeholder="Enter password.."/>
+        </div>
+    )
+}
+
+export default Password;
